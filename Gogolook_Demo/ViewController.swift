d1a0a5537be60ccc58f814d268bb9fa384e5fb8e
Helios Chen <Helios@chargesmith.com>
2022-07-18 12:59:41 +0800

    Initial Commit

---
+++ Gogolook_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gogolook_Demo
+//
+//  Created by Helios Chen on 2022/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
